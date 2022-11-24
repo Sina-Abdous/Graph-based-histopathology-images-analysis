31db5555dca27c361f9a17512ee285c40f60c46a
re0078 <reza.abdollahzadeh12@gmail.com>
2022-11-24 12:28:28 +0330

    install reconfiguration

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # install requirements
-pip install torch==1.9.1+cu111 torchaudio -f https://download.pytorch.org/whl/cu111/torch_stable.html
+pip install torch==1.9.1+cu113 torchaudio -f https://download.pytorch.org/whl/cu113/torch_stable.html
 # install torchaudio, thus fairseq installation will not install newest torchaudio and torch(would replace torch-1.9.1)
 pip install lmdb
-pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
-pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu111.html
+pip install torch-scatter==2.0.9 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu113.html
+pip install torch-sparse==0.6.12 -f https://pytorch-geometric.com/whl/torch-1.9.1+cu113.html
 # pip install torch-geometric==1.7.2  # original
 pip install torch-geometric==2.1.0.post1
 pip install tensorboardX==2.4.1
@@ -24,10 +24,10 @@ pip install setuptools==59.5.0
 # install submodules
 git submodule update --init --recursive
 (
-cd tokengt/large-scale-regression/fairseq || exit
+cd fairseq || exit
 pip install .
 python setup.py build_ext --inplace
-cd ../../.. || exit
+cd .. || exit
 )
 
 # prevent train hang
--- tokengt/large_scale_regression/scripts/bracs-lap-performer-finetune.sh
+++ tokengt/large_scale_regression/scripts/bracs-lap-performer-finetune.sh
@@ -1,32 +1,29 @@
-#!/usr/bin/env bash
-
 ulimit -c unlimited
 
 # Change dataset-name for each local run
 
 fairseq-train \
---user-dir ../tokengt \
---num-workers 16 \
---ddp-backend=legacy_ddp \
---dataset-name /home/re/Documents/Dr.RohbanLab/cell_graphs/ \  
---dataset-source dgl-local \
---task graph_prediction \
---criterion l1_loss \
---arch tokengt_base \
---lap-node-id \
---lap-node-id-k 16 \
---lap-node-id-sign-flip \
---performer \
---performer-finetune \
---performer-feature-redraw-interval 100 \
---prenorm \
---num-classes 1 \
---attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
---optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
---lr-scheduler cosine --warmup-updates 1000 --max-update 200000 \
---lr 2e-4 \
---batch-size 128 \
---data-buffer-size 20 \
---save-dir ./ckpts/bracs-tokengt-lap16-performer-finetune \
---tensorboard-logdir ./tb/bracs-tokengt-lap16-performer-finetune \
---no-epoch-checkpoints
+	--user-dir ../tokengt \
+	--num-workers 16 \
+	--ddp-backend=legacy_ddp \
+	--dataset-name /mnt/sda3/home/si_abdous/tokengt/dataset/hact-net-data/cell_graphs --dataset-source dgl-local \
+	--task graph_prediction \
+	--criterion l1_loss \
+	--arch tokengt_base \
+	--lap-node-id \
+	--lap-node-id-k 16 \
+	--lap-node-id-sign-flip \
+	--performer \
+	--performer-finetune \
+	--performer-feature-redraw-interval 100 \
+	--prenorm \
+	--num-classes 1 \
+	--attention-dropout 0.0 --act-dropout 0.1 --dropout 0.0 \
+	--optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
+	--lr-scheduler cosine --warmup-updates 1000 --max-update 200000 \
+	--lr 2e-4 \
+	--batch-size 128 \
+	--data-buffer-size 20 \
+	--save-dir ./ckpts/bracs-tokengt-lap16-performer-finetune \
+	--tensorboard-logdir ./tb/bracs-tokengt-lap16-performer-finetune \
+	--no-epoch-checkpoints 
